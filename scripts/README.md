da25a9f218a070b567f400e8fd4c0ebfecd4a5d6
Jackchows <jackchows@gmail.com>
2024-05-11 09:14:17 +0800

    Merge buildYaml.py into buildTxt.py

--- scripts/README.md
+++ scripts/README.md
@@ -8,13 +8,15 @@
 * **buildTxt.py**<br />
 > 參數
 ```
--s    需要轉換的源文件，如[Cangjie5.txt]
--o    輸出文件的字和倉頡碼的順序[char=字在前, code=倉頡碼在前]
--d    輸出文件的分隔符[tab=製表鍵, space=一個空格, multi=多個空格, none=無]
--l    輸出文件的換行符[crlf, cr, lf]
+-s source    需要轉換的源文件，如[Cangjie5.txt]
+-o order     輸出文件的字和倉頡碼的順序[char=字在前, code=倉頡碼在前]
+-d delimiter 輸出文件的分隔符[tab=製表鍵, space=一個空格, multi=多個空格, none=無]
+-l linebreak 輸出文件的換行符[crlf, cr, lf]
+-t template  使用模板生成包含配置項的碼表文件，此參數衹可以與-s共用
+             [rime=ibus-rime,weasel=小狼亳, squirrel=鼠鬚管, fcitx=Fcitx 5, yong=小小輸入法]
 ```
 > 示例
-1. 轉換`一般排序`碼表，字在前，倉頡碼在後，以`Tab`分隔，以`\r\n`換行（格式與小狼亳相同）
+1. 轉換`一般排序`碼表，字在前，倉頡碼在後，以`Tab`分隔，以`\r\n`換行（格式與小狼亳相同，但不包含配置項）
 ```
 python buildTxt.py -s Cangjie5.txt -o char -d tab -l crlf
 ```
@@ -26,7 +28,7 @@ python buildTxt.py -s Cangjie5.txt -o char -d tab -l crlf
 𫓣	caaf
 𨧹	cab
 ```
-2. 轉換`傳統漢字優先，偏好香港用字習慣`碼表，字在前，倉頡碼在後，以`Tab`分隔，以`\r`換行（格式與鼠鬚管相同）
+2. 轉換`傳統漢字優先，偏好香港用字習慣`碼表，字在前，倉頡碼在後，以`Tab`分隔，以`\r`換行（格式與鼠鬚管相同，但不包含配置項）
 ```
 python buildTxt.py -s Cangjie5_HK.txt -o char -d tab -l cr
 ```
@@ -38,7 +40,7 @@ python buildTxt.py -s Cangjie5_HK.txt -o char -d tab -l cr
 𫓣	caaf
 𨧹	cab
 ```
-3. 轉換`簡化字優先`碼表，倉頡碼在前，字在後，以`Space`分隔並對齊，以`\r\n`換行（格式與小小輸入法平台及「倉頡平台 2022」相同）
+3. 轉換`簡化字優先`碼表，倉頡碼在前，字在後，以`Space`分隔並對齊，以`\r\n`換行（格式與小小輸入法平台及「倉頡平台 2022」相同，但不包含配置項）
 ```
 python buildTxt.py -s Cangjie5_SC.txt -o code -d multi -l crlf
 ```
@@ -49,6 +51,10 @@ ca      鈤
 caa     錩
 caaf    𫓣
 cab     𨧹
+```
+4. 使用模板轉換`一般排序`碼表，生成適用於小狼亳輸入法的`.dict.yaml`文件（使用模板時，生成的文件會**包含配置項**）
+```
+python buildTxt.py -s Cangjie5.txt -t weasel
 ```
 
 * **buildYaml.py**<br />
--- scripts/buildTxt.py
+++ scripts/buildTxt.py
@@ -1,14 +1,14 @@
 # -*- coding: utf8 -*-
 import os
 import re
-#import time
+import datetime
 import argparse
 import platform
 
 def chooseSource(source=None):                                    # 輸入文件
     if source is None:                                              # 如果沒有使用--source參數，則交互式提示
         print("此工具可以將碼表轉換成所需的格式，你可以選擇字和倉頡碼的順序、分隔符以及換行符。")
-        print("Step 1 - 選擇需要轉換的文件")
+        print("\nStep 1 - 選擇需要轉換的文件")
         print("[1] Cangjie5.txt")
         print("[2] Cangjie5_TC.txt")
         print("[3] Cangjie5_HK.txt")
@@ -34,7 +34,7 @@ def chooseSource(source=None):                                    # 輸入文件
 
 def chooseOrder(order=None):                                      # 順序
     if order is None:                                               # 如果沒有使用--order參數，則交互式提示
-        print("Step 2 - 選擇字和倉頡碼的順序")
+        print("\nStep 2 - 選擇字和倉頡碼的順序")
         print("[1] 字在前，倉頡碼在後")
         print("[2] 倉頡碼在前，字在後")
         order_input=input("輸入數字並按Enter (默認為1):")
@@ -58,7 +58,7 @@ def chooseOrder(order=None):                                      # 順序
 
 def chooseDelimiter(delimiter=None):                              # 分隔符
     if delimiter is None:                                           # 如果沒有使用--delimiter參數，則交互式提示
-        print("Step 3 - 選擇字與倉頡碼之間的分隔符")
+        print("\nStep 3 - 選擇字與倉頡碼之間的分隔符")
         print("[1] Tab")
         print("[2] Space (一個空格)")
         print("[3] Spaces (以空格代替Tab對齊)")
@@ -92,7 +92,7 @@ def chooseDelimiter(delimiter=None):                              # 分隔符
 
 def chooseLineBreak(linebreak=None):                              # 換行符
     if linebreak is None:                                           # 如果沒有使用--linebreak參數，則交互式提示
-        print("Step 4 - 選擇輪出文件的換行符")
+        print("\nStep 4 - 選擇輪出文件的換行符")
         print("[1] \\r\\n")
         print("[2] \\n")
         print("[3] \\r")
@@ -133,20 +133,26 @@ def chooseLineBreak(linebreak=None):                              # 換行符
         exit()
     return linebreak
 
-def buildTxt(source,order,delimiter,linebreak):
+def buildTxt(source,order,delimiter,linebreak,output=None):                   # 默認不使用模板
 
     # 獲取
-    current_directory = os.path.dirname(os.path.abspath(__file__))    # 獲取文件目錄
-    parent_directory = os.path.dirname(current_directory)             # 獲取上級目錄
+    # current_directory = os.path.dirname(os.path.abspath(__file__))    # 獲取文件目錄
+    # parent_directory = os.path.dirname(current_directory)             # 獲取上級目錄
     source_file = os.path.join(parent_directory,source)
     if (os.path.exists(source_file)==False):                               # 若source_file不存在
             print ("未找到 "+str(source_file)+"，請檢查文件是否存在")
             return
     #output_file = current_directory+'\\'+'converter_output_'+time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))+'.txt'
-    output_file = os.path.join(current_directory,source.replace('.txt','_formatted.txt'))
+    if output is None:
+        output_file = os.path.join(current_directory,source.replace('.txt','_formatted.txt'))    # 不使用模板
+    else:
+        output_file = output                                                                     # 使用模板
     # 開啟
     with open(source_file,'r',encoding='utf8') as gib, \
-         open(output_file,'w',encoding='utf8',newline='') as txt:
+         open(output_file,'a',encoding='utf8',newline='') as txt:
+        if output is None:                                                     # 不使用模板
+            txt.seek(0,0)
+            txt.truncate()
         # 寫入
         value_pattern = re.compile(r'^([^\t\n\r]+)\t([a-z]{1,5})')
         for line in gib:
@@ -172,12 +178,144 @@ def buildTxt(source,order,delimiter,linebreak):
     # 關閉
     print("完成。輸出文件 "+output_file)
 
+def buildYaml(source,template):                                               # RIME 模板
+
+    yaml_file = os.path.join(current_directory,source.replace('.txt','.dict.yaml').lower())
+    if template == 'weasel':
+        linebreak="\r\n"
+    elif template == 'squirrel':
+        linebreak="\r"
+    elif template == 'rime':
+        linebreak="\n"
+    order = 'char'
+    delimiter = '\t'
+
+    description = {}
+    description['Cangjie5.txt']='一般排序，綜合考慮字頻及繁簡，部分常用簡化字可能排在傳統漢字前面。'
+    description['Cangjie5_TC.txt']='傳統漢字優先，偏好台灣用字習慣，符合《常用國字標準字體表》的字形將排在前面。'
+    description['Cangjie5_HK.txt']='傳統漢字優先，偏好香港用字習慣，符合《常用字字形表》的字形將排在前面。'
+    description['Cangjie5_SC.txt']='簡化字優先，符合《通用規範漢字表》的字形將排在前面。'
+    description['Cangjie5_special.txt']='收字較少的版本，收錄主流系統通常可以顯示的字符。'
+    schema_id = source.replace('.txt','').lower()
+
+    # 開頭
+    yaml_head ='''# encoding: utf-8
+#
+# 倉頡五代補完計劃：
+# https://github.com/Jackchows/Cangjie5
+# 使用前務必閱讀：
+# https://github.com/Jackchows/Cangjie5/blob/master/README.md 及
+# https://github.com/Jackchows/Cangjie5/blob/master/change_summary.md
+# 
+# 相關項目：倉頡三代補完計畫
+# https://github.com/Arthurmcarthur/Cangjie3-Plus
+#
+# '''+str(description[source])+'''
+---
+name: "'''+str(schema_id)+'''"
+version: "'''+str(datetime.datetime.now().strftime('%Y.%m.%d'))+'''"
+sort: original
+use_preset_vocabulary: true
+max_phrase_length: 1
+columns:
+  - text
+  - code
+  - stem
+encoder:
+  exclude_patterns:
+    - '^x.*$'
+    - '^z.*$'
+  rules:
+    - length_equal: 2
+      formula: "AaAzBaBbBz"
+    - length_equal: 3
+      formula: "AaAzBaBzCz"
+    - length_in_range: [4, 10]
+      formula: "AaBzCaYzZz"
+  tail_anchor: "'"
+...
+
+''' 
+    # 寫入yaml
+    with open(yaml_file,'w',encoding='utf8') as yal:
+        yal.write(yaml_head)
+    buildTxt(source,order,delimiter,linebreak,yaml_file)
+    
+def buildYong(source):                                                        # 小小輸入法模板
+    yong_file = os.path.join(current_directory,source.lower())
+    linebreak="\r\n"
+    order = 'code'
+    delimiter = '\t'
+    # 開頭
+    yaml_head ='''#-----------------------------------------------------------------
+# 倉頡五代補完計劃：
+# https://github.com/Jackchows/Cangjie5
+# 使用前務必閱讀：
+# https://github.com/Jackchows/Cangjie5/blob/master/README.md 及
+# https://github.com/Jackchows/Cangjie5/blob/master/change_summary.md
+#
+# 相關項目：倉頡三代補完計畫
+# https://github.com/Arthurmcarthur/Cangjie3-Plus
+#
+# 一般排序，綜合考慮字頻及繁簡，部分常用簡化字可能排在傳統漢字前面。
+#-----------------------------------------------------------------
+encode=UTF-8
+name=五倉補完
+key=abcdefghijklmnopqrstuvwxyz
+len=6
+wildcard=*
+commit=1 6 0
+#dicts=mb/cj5-ftzk.txt
+#dicts=mb/cj5-jtzk.txt
+#assist=mb/assist/pinyin.txt
+
+[DATA]
+'''
+    # 寫入yong
+    with open(yong_file,'w',encoding='utf8') as yog:
+        yog.write(yaml_head)
+    buildTxt(source,order,delimiter,linebreak,yong_file)
+
+def buildFcitx(source):                                                       # Fcitx 5 模板
+    fcitx_file = os.path.join(current_directory,source.lower().replace('.txt','_fcitx.txt'))
+    linebreak="\n"
+    order = 'code'
+    delimiter = ' '
+    fcitx_head ='''键码=abcdefghijklmnopqrstuvwxyz
+码长=6
+[数据]
+'''
+    # 寫入fcitx
+    with open(fcitx_file,'w',encoding='utf8',newline = '\n') as fcx:
+        fcx.write(fcitx_head.replace('\r\n','\n'))
+    buildTxt(source,order,delimiter,linebreak,fcitx_file)
+
+def buildWithTemplate(template,source):                                  # 判斷是否使用模板
+    if template == 'rime':
+        buildYaml(source,template)
+        return 'yes'
+    elif template == 'weasel':
+        buildYaml(source,template)
+        return 'yes'
+    elif template == 'squirrel':
+        buildYaml(source,template)
+        return 'yes'
+    elif template == 'fcitx':
+        buildFcitx(source)
+        return 'yes'
+    elif template == 'yong':
+        buildYong(source)
+        return 'yes'
+    else:
+        return 'no'
+
 def parse_args():
     parse = argparse.ArgumentParser(description='參數')
     parse.add_argument('-s', '--source', help='需要轉換的源文件，如[Cangjie5.txt]')  # 創建參數
     parse.add_argument('-o', '--order', help='字和倉頡碼的順序[char=字在前, code=倉頡碼在前]')
     parse.add_argument('-d', '--delimiter', help='分隔符[tab=製表鍵, space=一個空格, multi=以空格代替Tab對齊, none=無]')
     parse.add_argument('-l', '--linebreak', help='換行符[crlf, cr, lf]')
+    parse.add_argument('-t', '--template', help='使用模板[rime=ibus-rime, weasel=小狼亳, squirrel=鼠鬚管, fcitx=Fcitx 5, yong=小小輸入法]')
     args = parse.parse_args()                   # 解析參數
     return args
 
@@ -187,10 +325,23 @@ if __name__ == "__main__":
     order = args.order
     delimiter = args.delimiter
     linebreak = args.linebreak
-    
-    source=chooseSource(source)
-    order=chooseOrder(order)
-    delimiter=chooseDelimiter(delimiter)
-    linebreak=chooseLineBreak(linebreak)
+    template = args.template
 
-    buildTxt(source,order,delimiter,linebreak)
+    current_directory = os.path.dirname(os.path.abspath(__file__))    # 獲取文件目錄
+    parent_directory = os.path.dirname(current_directory)             # 獲取上級目錄
+    
+    if template:
+        if (order is not None) | (delimiter is not None) | (linebreak is not None):
+            print('--template 參數衹可以與 --source 參數共用')
+            exit()
+
+    build_with_template = buildWithTemplate(template,source)   # 判斷是否使用模板
+
+    if build_with_template == 'no':                     # 不使用模板
+        source=chooseSource(source)
+        order=chooseOrder(order)
+        delimiter=chooseDelimiter(delimiter)
+        linebreak=chooseLineBreak(linebreak)
+        buildTxt(source,order,delimiter,linebreak)
+
+    
