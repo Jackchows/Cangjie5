0a953d1c42e385cbc226905e39892772adaed4d0
ngivanyh <103019223+ngivanyh@users.noreply.github.com>
2025-12-06 21:44:25 +0800

    簡化和優化程式嗎

--- scripts/buildJson.py
+++ scripts/buildJson.py
@@ -1,56 +1,45 @@
-# -*- coding: utf8 -*-
-import os
 import re
 import json
+from pathlib import Path
 
-def buildJson(github):
+def buildJson(github: str):
     # 獲取
-    current_directory = os.path.dirname(os.path.abspath(__file__))    # 獲取文件目錄
-    parent_directory = os.path.dirname(current_directory)             # 獲取上級目錄
-    github_file = os.path.join(parent_directory,github)
-    # print(str(github_file))
-    if (os.path.exists(github_file)==False):                               # 若github不存在
-        # print ("["+str(github)+"] does not exists.")
-        print ("未找到 "+str(github_file)+"，請檢查文件是否存在")
+    current_directory = Path.cwd() # 獲取文件目錄
+    parent_directory = current_directory.parent # 獲取上級目錄
+    github_file = parent_directory / github
+    if not github_file.exists(): # 若 github 不存在
+        print (f"未找到 {github_file}，請檢查文件是否存在")
         return
-    #output_file = current_directory+'/'+'converter_output_'+time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))+'.json'
-    #json_file = current_directory+'/'+github.replace('.txt','.json')
-    json_file = os.path.join(current_directory,github.replace('.txt','.json'))
+    # output_file = current_directory+'/'+'converter_output_'+time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))+'.json'
+    # json_file = current_directory+'/'+github.replace('.txt','.json')
+    json_file = current_directory / github.replace('.txt','.json')
 
     # 開啟
-    with open(github_file,'r',encoding='utf8') as gib,\
-         open(json_file,'w',encoding='utf8') as jsn:
+    with open(github_file, 'r', encoding='utf8') as gib,\
+         open(json_file, 'w', encoding='utf8') as jsn:
         # 寫入
         data = {}
 
         value_pattern = re.compile(r'^([^\t\n\r]+)\t([a-z]{1,5})')
         for line in gib:
             # value_line = re.match(r'^([^\t\n\r]+)\t([a-z]{1,5})', line)    # 排除開頭的說明文字
-            value_line = value_pattern.match(line)                         # 排除開頭的說明文字
-            if value_line:
-                value_char = value_line[1]
-                value_code = value_line[2]
-                if value_code in data:
-                    data[value_code].append(value_char)
+            if out := value_pattern.match(line): # 排除開頭的說明文字
+                char, code = out[1], out[2] # 取得輸出結果的前兩個 group
+                
+                if code in data:
+                    data[code].append(char)
                 else:
-                    data[value_code] = [value_char]
+                    data[code] = [char]
 
-        json_data = json.dumps(data, ensure_ascii=False, indent=2, check_circular=False)
-        jsn.write(json_data)
+        jsn.write(json.dumps(data, ensure_ascii=False, indent=2, check_circular=False))
     
     # 結束
-    print("輸出文件 "+json_file)
+    print(f"輸出文件 {json_file}")
 
 if __name__ == "__main__":
-    github = 'Cangjie5.txt'
-    buildJson(github)
-    github = 'Cangjie5_TC.txt'
-    buildJson(github)
-    github = 'Cangjie5_HK.txt'
-    buildJson(github)
-    github = 'Cangjie5_SC.txt'
-    buildJson(github)
-    github = 'Cangjie5_special.txt'
-    buildJson(github)
+    PROCESS_FILES = ('Cangjie5.txt', 'Cangjie5_TC.txt', 'Cangjie5_HK.txt', 'Cangjie5_SC.txt', 'Cangjie5_special.txt')
+    
+    for file in PROCESS_FILES:
+        buildJson(file)
 
     print("完成。")
